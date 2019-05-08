96ea8e3e2bc4ee14a6faef362d7bd432818775ee
takuyamurayama <48488553+takuyamurayama@users.noreply.github.com>
2019-05-08 19:53:28 +0900

    updateall->save 　findall->findfirst

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -49,47 +49,59 @@ class UsersController extends AppController {
 
 	public function mail() {
 		if ($this->request->is('post') || $this->request->is('put')) {
-			if(isset($this->request->data['reset'])) {
-				$reset = $this->request->data['reset'];
-				date_default_timezone_set('Asia/Tokyo');
-				$date = date("Y-m-d H:i", strtotime("+30 minute"));
-				$param = (md5(uniqid(rand(), true)));
-				$url = "https://procir-study.site/murayama/cake/cakephp-2.x/users/pass_reset/?name=$param";
-				$email_count = $this->User->find('count', array(
-					'conditions' => array(
-						'email' => $reset
-					),
-				)
-			);
-				if($email_count === 1) {
-					$email = new CakeEmail('gmail');
-					$email->config(array('log' => true));
-					$email->from(array('takuya.msb@gmail.com' => 'takuya in the sky'));
-					$email->to($reset);
-					$email->subject('高庭さんへ');
-					$email->send($url);
-					$this->User->updateAll(
-						array(
-							'User.param' => '"' . $param . '"',
-							'User.date' => '"' . $date . '"'
+			$this->User->set( $this->request->data );
+			if ($this->User->validates()) {
+				if(isset($this->request->data['reset'])) {
+					$reset = $this->request->data['reset'];
+					date_default_timezone_set('Asia/Tokyo');
+					$date = date("Y-m-d H:i", strtotime("+30 minute"));
+					$param = (md5(uniqid(rand(), true)));
+					$url = "https://procir-study.site/murayama/cake/cakephp-2.x/users/pass_reset/?name=$param";
+					$email_count = $this->User->find('count', array(
+						'conditions' => array(
+							'email' => $reset
 						),
-						array(
-							'User.email' => $reset,
+					)
+				);
+					$id = $this->User->find('first', array(
+						'conditions' => array(
+							'email' => $reset
 						)
-					);
-					$this->Session->setFlash(__("再発行用URLを送信しました"));
-					$this->redirect(array('controller' => 'posts', 'action' => 'index'));
-				} else {
-					$this->Session->setFlash(__("再発行用URLを送信しました)"));
-					$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+					)
+				);
+					$id = $id['User']['id'];
+					if($email_count === 1) {
+						$email = new CakeEmail('gmail');
+						$email->config(array('log' => true));
+						$email->from(array('takuya.msb@gmail.com' => 'takuya in the sky'));
+						$email->to($reset);
+						$email->subject('再発行URLです');
+						$email->send($url);
+						$data = array(
+							'User' => array(
+								'id' => $id,
+								'param' => $param,
+								'date' => $date,
+							)
+						);
+						$this->User->save($data);
+						$this->Session->setFlash(__("再発行用URLを送信しました"));
+						$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+					} else {
+						$this->Session->setFlash(__("再発行用URLを送信しました"));
+						$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+					}
 				}
+			} else {
+				$this->Session->setFlash(__("再発行URLを送信しました"));
+				$this->redirect(array('controller' => 'posts', 'action' => 'index'));
 			}
 		}
 	}
 
 	public function pass_reset() {
 		$param = $this->request->query(array('name'));
-		$all = $this->User->find('all', array(
+		$all = $this->User->find('first', array(
 			'conditions' => array(
 				'param' => $param
 			)
@@ -106,42 +118,46 @@ class UsersController extends AppController {
 			$this->redirect(array('controller' => 'posts', 'action' => 'index'));
 
 		} else {
-			$this->set('id', $all['0']['User']['id']);
+			$this->set('id', $all['User']['id']);
 			if ($this->request->is('post') || $this->request->is('put')) {
-				if (!empty($this->request->data['User']['pass'])) {
-					$hidden = $this->request->data['User']['id'];
-					$reset = $this->request->data['User']['pass'];
-					$new_pass = AuthComponent::password($reset);
-					$time = date("Y-m-d H:i");
-					$date = $all['0']['User']['date'];
-					$total = $this->User->find('count', array(
-						'conditions' => array(
-							'id' => $hidden,
-							'param' => $param
+				$this->User->set($this->request->data);
+				if ($this->User->validates()) {
+					if (!empty($this->request->data['User']['pass'])) {
+						$hidden = $this->request->data['User']['id'];
+						$reset = $this->request->data['User']['pass'];
+						Security::hash( $reset, 'blowfish');
+						$time = date("Y-m-d H:i");
+						$date = $all['User']['date'];
+						$total = $this->User->find('count', array(
+							'conditions' => array(
+								'id' => $hidden,
+								'param' => $param
+							)
 						)
-					)
-				);
-					if ($total === 1) {
-						if(strtotime($time) <= strtotime($date)) {
-							$this->User->updateAll(
-								array(
-									'User.password' => '"' . $new_pass . '"',
-									'User.date' => '0'
-								),
-								array(
-									'User.param' => $param,
-								)
-							);
-							$this->Session->setFlash(__("パスワードが変更されました"));
-							$this->redirect(array('controller' => 'posts', 'action' => 'index'));
-						} else {
-							$this->Session->setFlash(__("30分以上経過しているか、すでに変更しています"));
-							$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+					);
+						if ($total === 1) {
+							if(strtotime($time) <= strtotime($date)) {
+								$data = array(
+									'User' => array(
+										'id' => $hidden,
+										'date' => '0',
+										'password' => $reset,
+									)
+								);
+								$this->User->save($data);
+								$this->Session->setFlash(__(" $hidden . パスワードが変更されました"));
+								$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+							} else {
+								$this->Session->setFlash(__("30分以上経過しているか、すでに変更しています"));
+								$this->redirect(array('controller' => 'posts', 'action' => 'index'));
 
+							}
+						} else {
+							$this->Session->setFlash(__("不正なアクセス"));
+							$this->redirect(array('controller' => 'posts', 'action' => 'index'));
 						}
 					} else {
-						$this->Session->setFlash(__("不正なアクセス"));
-						$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+						$this->Session->setFlash(__("１文字以上入れてください"));
 					}
 				} else {
 					$this->Session->setFlash(__("１文字以上入れてください"));
@@ -151,31 +167,95 @@ class UsersController extends AppController {
 	}
 
 
-	public function user_info($id) {
-		$user_info = $this->User->findById($id);
-		$this->set('user_info', $user_info);
-		$pic = $this->User->findById($id);
-		$this->set('pic', $pic);
-		$email = $this->Session->read('email');
-		$this->set('email', $email);
-		$this->set('id', $id);
-		$total = $this->User->find('count', array(
-			'conditions' => array(
-				'id' => $id,
-				'email' => $email
-			),
-		)
-	);
-		$this->set('total', $total);
-	}
-
-	public function user_pic($id = null) {
-		if ($this->request->is('post') || $this->request->is('put')) {
-			$this->User->set($this->data);
-			$this->User->validates();
-			$this->User->id = $id;
+		public function user_info($id) {
+			$user_info = $this->User->findById($id);
+			$this->set('user_info', $user_info);
+			$pic = $this->User->findById($id);
+			$this->set('pic', $pic);
 			$email = $this->Session->read('email');
-			$tmp =  $this->request->data['User']['image']['tmp_name'];
+			$this->set('email', $email);
+			$this->set('id', $id);
+			$total = $this->User->find('count', array(
+				'conditions' => array(
+					'id' => $id,
+					'email' => $email
+				),
+			)
+		);
+			$this->set('total', $total);
+		}
+
+		public function user_pic($id = null) {
+			if ($this->request->is('post') || $this->request->is('put')) {
+				$this->User->set($this->data);
+				$this->User->validates();
+				$this->User->id = $id;
+				$email = $this->Session->read('email');
+				$tmp =  $this->request->data['User']['image']['tmp_name'];
+				$total = $this->User->find('count', array(
+					'conditions' => array(
+						'id' => $id,
+						'email' => $email
+					)
+				)
+			);
+				if ($total === 1) {
+					if(isset($this->request->data['delete'])) {
+						$image = array(
+							'User' => array(
+								'image' => null,
+							)
+						);
+						if ($this->User->save($image)) {
+							$this->Session->setFlash(__('画像が削除されました'));
+							$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+						}
+					} elseif (is_uploaded_file($tmp)) {
+						$file_name = $this->request->data['User']['image']['name'];
+						$this->User->set($file_name);
+						if($this->User->validates()){
+							$new = uniqid();
+							rename($file_name, $new);
+							$file =  WWW_ROOT . 'img' . DS . $new;
+							if (move_uploaded_file($tmp, $file)) {
+								$image = array(
+									'User' => array(
+										'image' => $new,
+									)
+								);
+								if ($this->User->save($image, false)) {
+									$this->Session->setFlash(__('画像が保存されました'));
+									$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+								} else {
+									$this->Session->setFlash(__('画像が保存できませんでした'));
+								}
+							}
+						}else{
+							$this->Session->setFlash(__('バリデーションエラー'));
+							$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+						}
+					} else {
+						echo "正しくアップロードされていません";
+					}
+				} else {
+					echo "ログインが不正の可能性があります";
+				}
+				if (!$this->request->data) {
+					$this->request->data = $post;
+				}
+			}
+		}
+
+		public function user_info_edit($id = null) {
+			$email = $this->Session->read('email');
+			if (!$id) {
+				throw new NotFoundException(__('不正なID'));
+			}
+
+			$post = $this->User->findById($id);
+			if (!$post) {
+				throw new NotFoundException(__('不正な投稿'));
+			}
 			$total = $this->User->find('count', array(
 				'conditions' => array(
 					'id' => $id,
@@ -184,104 +264,40 @@ class UsersController extends AppController {
 			)
 		);
 			if ($total === 1) {
-				if(isset($this->request->data['delete'])) {
-					$image = array(
-						'User' => array(
-							'image' => null,
-						)
-					);
-					if ($this->User->save($image)) {
-						$this->Session->setFlash(__('画像が削除されました'));
-						$this->redirect(array('controller' => 'posts', 'action' => 'index'));
+				if ($this->request->is(array('post', 'put'))) {
+					$this->User->id = $id;
+					if ($this->User->save($this->request->data)) {
+						$this->Flash->success(__('コメントがアップデートされました'));
+						return $this->redirect(array('controller' => 'Posts', 'action' => 'index'));
 					}
-				} elseif (is_uploaded_file($tmp)) {
-					$file_name = $this->request->data['User']['image']['name'];
-					$this->User->set($file_name);
-					if($this->User->validates()){
-						$new = uniqid();
-						rename($file_name, $new);
-						$file =  WWW_ROOT . 'img' . DS . $new;
-						if (move_uploaded_file($tmp, $file)) {
-							$image = array(
-								'User' => array(
-									'image' => $new,
-								)
-							);
-							if ($this->User->save($image, false)) {
-								$this->Session->setFlash(__('画像が保存されました'));
-								$this->redirect(array('controller' => 'posts', 'action' => 'index'));
-							} else {
-								$this->Session->setFlash(__('画像が保存できませんでした'));
-							}
-						}
-					}else{
-						$this->Session->setFlash(__('バリデーションエラー'));
-						$this->redirect(array('controller' => 'posts', 'action' => 'index'));
-					}
-				} else {
-					echo "正しくアップロードされていません";
+					$this->Flash->error(__('コメントをアップデートできませんでした'));
 				}
 			} else {
-				echo "ログインが不正の可能性があります";
+				echo "不正なアクセスです";
 			}
 			if (!$this->request->data) {
 				$this->request->data = $post;
 			}
 		}
-	}
 
-	public function user_info_edit($id = null) {
-		$email = $this->Session->read('email');
-		if (!$id) {
-			throw new NotFoundException(__('不正なID'));
+		public function view($id = null) {
+			$this->User->id = $id;
+			if (!$this->User->exists()) {
+				throw new NotFoundException(__('不正なユーザーです'));
+			}
+			$this->set('user', $this->User->findById($id));
 		}
 
-		$post = $this->User->findById($id);
-		if (!$post) {
-			throw new NotFoundException(__('不正な投稿'));
-		}
-		$total = $this->User->find('count', array(
-			'conditions' => array(
-				'id' => $id,
-				'email' => $email
-			)
-		)
-	);
-		if ($total === 1) {
-			if ($this->request->is(array('post', 'put'))) {
-				$this->User->id = $id;
+		public function add() {
+			if ($this->request->is('post')) {
+				$this->User->create();
 				if ($this->User->save($this->request->data)) {
-					$this->Flash->success(__('コメントがアップデートされました'));
-					return $this->redirect(array('controller' => 'Posts', 'action' => 'index'));
+					$this->Flash->success(__('会員登録されました'));
+					$this->redirect($this->Auth->redirect());
 				}
-				$this->Flash->error(__('コメントをアップデートできませんでした'));
+				$this->Flash->error(
+					__('会員登録できませんでした')
+				);
 			}
-		} else {
-			echo "不正なアクセスです";
-		}
-		if (!$this->request->data) {
-			$this->request->data = $post;
 		}
 	}
-
-	public function view($id = null) {
-		$this->User->id = $id;
-		if (!$this->User->exists()) {
-			throw new NotFoundException(__('不正なユーザーです'));
-		}
-		$this->set('user', $this->User->findById($id));
-	}
-
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->User->create();
-			if ($this->User->save($this->request->data)) {
-				$this->Flash->success(__('会員登録されました'));
-				$this->redirect($this->Auth->redirect());
-			}
-			$this->Flash->error(
-				__('会員登録できませんでした')
-			);
-		}
-	}
-}
